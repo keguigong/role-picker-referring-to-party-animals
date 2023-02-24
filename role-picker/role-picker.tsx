0200449f0bdeec1febe1f7ba77e140907b449ef0
可圭共 <keguigong@gmail.com>
2023-02-24 23:16:20 +0800

    Add arrowkey event and center active item

--- role-picker/role-picker.tsx
+++ role-picker/role-picker.tsx
@@ -1,118 +1,154 @@
-import { MouseEvent, TouchEvent, useEffect, useRef, useState } from "react"
-import classNames from "classnames"
+import { useEffect, useRef, useState, useCallback } from "react";
+import classNames from "classnames";
 
-import styles from "./styles.module.scss"
-import RoleAvatar from "./role-avatar"
-import rolesList from "./roles.json"
+import styles from "./styles.module.scss";
+import RoleAvatar from "./role-avatar";
+import rolesList from "./roles.json";
 
 export default function RolePicker() {
-  const [divLeft, setDivLeft] = useState(0)
-  const [startX, setStartX] = useState(0)
-  const [dragFlag, setDragFlag] = useState(false)
+  const domRef = useRef<HTMLUListElement>(null);
+  const conRef = useRef<HTMLDivElement>(null);
 
-  const [innerWidth, setInnerWidth] = useState(0)
-  const [conWidth, setConWidth] = useState(0)
+  const [moveFlag, setFlag] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [left, setLeft] = useState(0);
 
-  const containerEl = useRef<HTMLDivElement>(null)
-  const innerEl = useRef<HTMLUListElement>(null)
-
-  useEffect(() => {
-    if (!containerEl.current || !innerEl.current) return
-    setInnerWidth(innerEl.current.scrollWidth)
-    setConWidth(containerEl.current.offsetWidth)
-  }, [])
-
-  function moveX(deltaX: number) {
-    let newValue =
-      divLeft - deltaX > 0
-        ? divLeft - deltaX < innerWidth - conWidth
-          ? divLeft - deltaX
-          : innerWidth - conWidth
-        : 0
-    setDivLeft(newValue)
-  }
+  const [activeIndex, setActive] = useState(0);
+  const [arrowLeft, setArrowLeft] = useState(-1);
 
   // Mouse events
-  function handleMoveStart() {
-    setDragFlag(true)
-  }
+  const slideCallback = useCallback(
+    (e: MouseEvent | TouchEvent) => {
+      if (domRef.current && conRef.current && moveFlag) {
+        const deltaX =
+          e instanceof TouchEvent ? e.touches[0].clientX - startX : e.movementX;
+        e instanceof TouchEvent && setStartX(e.touches[0].clientX);
+        const conWidth = conRef.current.clientWidth;
+        const width = domRef.current.scrollWidth;
+        const newLeft =
+          left + deltaX <= 0
+            ? left + deltaX >= conWidth - width
+              ? left + deltaX
+              : conWidth - width
+            : 0;
+        domRef.current.style.transform = `translateX(${newLeft}px)`;
+        setLeft(newLeft);
+      }
+    },
+    [domRef, conRef, moveFlag, left, startX]
+  );
 
-  function handleMove(e: MouseEvent) {
-    if (innerWidth - conWidth <= 0) return
-    if (dragFlag) {
-      moveX(e.movementX)
-    }
-  }
+  useEffect(() => {
+    const mousedown = () => setFlag(true);
+    const mouseup = () => setFlag(false);
+    const block = domRef.current;
+    block?.addEventListener("mousedown", mousedown);
+    window.addEventListener("mousemove", slideCallback);
+    window.addEventListener("mouseup", mouseup);
 
-  function handleMoveEnd() {
-    setDragFlag(false)
-    setStartX(0)
-  }
+    const touchstart = (e: TouchEvent) => (
+      setFlag(true), setStartX(e.touches[0].clientX)
+    );
+    const touchend = (e: TouchEvent) => setFlag(true);
+    block?.addEventListener("touchstart", touchstart);
+    window.addEventListener("touchmove", slideCallback);
+    window.addEventListener("touchend", touchend);
 
-  // Touch events
-  function handleTouchStart(e: TouchEvent) {
-    setDragFlag(true)
-    setStartX(e.touches[0].clientX)
-  }
+    return () => {
+      block?.removeEventListener("mousedown", mousedown);
+      window.removeEventListener("mousemove", slideCallback);
+      window.removeEventListener("mouseup", mouseup);
 
-  function handleTouchMove(e: TouchEvent) {
-    let x = e.touches[0].clientX
-    let deltaX = x - startX
-    moveX(deltaX)
-    setStartX(x)
-  }
-
-  // Click event
-  const [picked, setPicked] = useState(0)
-
-  function handlePick(index: number) {
-    setPicked(index)
-  }
+      block?.removeEventListener("touchstart", touchstart);
+      window.removeEventListener("touchmove", slideCallback);
+      window.removeEventListener("touchend", touchend);
+    };
+  }, [domRef, slideCallback]);
 
   // Keyboard events
-  // const savedHandler = useRef<(e: any) => void>()
-  // function handler(e: KeyboardEvent) {
-  //   console.log("key: ", e.code)
-  // }
+  const keyCallback = useCallback((e: KeyboardEvent) => {
+    const len = rolesList.length * 2;
+    if (e.code === "ArrowLeft")
+      setActive((prev) => (prev - 1 >= 0 ? prev - 1 : 0)), setArrowLeft(1);
+    else if (e.code === "ArrowRight")
+      setActive((prev) => (prev + 1 <= len - 1 ? prev + 1 : len - 1)),
+        setArrowLeft(-1);
+    else return;
+  }, []);
 
-  // useEffect(() => {
-  //   savedHandler.current = handler
-  //   // @ts-ignore
-  //   document.addEventListener("keydown", savedHandler.current)
-  //   return () => {
-  //     // @ts-ignore
-  //     document.removeEventListener("keydown", savedHandler.current)
-  //   }
-  // }, [handler])
+  useEffect(() => {
+    if (!domRef.current || !conRef.current) return;
+    const len = rolesList.length * 2;
+
+    const conWidth = conRef.current.clientWidth;
+    const width = domRef.current.scrollWidth;
+
+    const liWidth = (width - 16) / len;
+    const liActiveWidth = liWidth + 16;
+
+    const halfConWidth = conWidth / 2;
+    const leftWidth = liWidth * activeIndex + liActiveWidth / 2;
+    const rightWidth = liWidth * (len - 1 - activeIndex) + liActiveWidth / 2;
+
+    // console.log(activeIndex, liWidth, leftWidth, halfConWidth);
+
+    if (leftWidth >= halfConWidth && rightWidth >= halfConWidth) {
+      setLeft(halfConWidth - leftWidth);
+    } else if (leftWidth < halfConWidth) setLeft(0);
+    else if (rightWidth < halfConWidth) setLeft(conWidth - width);
+  }, [domRef, conRef, activeIndex, arrowLeft]);
+
+  useEffect(() => {
+    if (domRef.current)
+      domRef.current.style.transform = `translateX(${left}px)`;
+  }, [domRef, left]);
+
+  useEffect(() => {
+    window.addEventListener("keydown", keyCallback);
+    return () => {
+      window.removeEventListener("keydown", keyCallback);
+    };
+  }, [keyCallback]);
+
+  function toggle(index: number) {
+    setActive(index);
+  }
+
+  useEffect(() => {
+    const springWobbly = (t: number) => -0.5 * (2.71828 ** (-6 * t)) * (
+      -2 * (2.71828 ** (6 * t)) + Math.sin(12 * t) + 2 * Math.cos(12 * t))
+
+    const lerp = (a: number, b: number, p: number) => a + p * (b - a)
+    let str = ""
+    for (let i = 0; i < 117; i++) {
+      let p = springWobbly(i / 100)
+      str += `${i - 17}% { transform: translateX(${lerp(100, 0, p)}px)}`
+    }
+    console.log(str)
+  }, [])
 
   return (
     <div tabIndex={0} className={styles["roles-container"]}>
-      <div
-        className={styles["left-mask"]}
-        onMouseDown={handleMoveStart}
-        onMouseMove={handleMove}
-        onMouseUp={handleMoveEnd}
-        onTouchStart={handleTouchStart}
-        onTouchMove={handleTouchMove}
-        onTouchEnd={handleMoveEnd}
-      >
-        <div className={styles["right-mask"]} ref={containerEl}>
+      <div className={styles["left-mask"]}>
+        <div className={styles["right-mask"]} ref={conRef}>
           <ul
-            className={classNames(styles["source-item-wrap"])}
-            style={{ transform: `translateX(-${divLeft}px)` }}
-            ref={innerEl}
+            className={classNames(
+              styles["source-item-wrap"],
+              !moveFlag && styles["transition"]
+            )}
+            ref={domRef}
           >
             {rolesList.concat(rolesList).map((role, index) => (
               <RoleAvatar
                 key={index}
                 name={role}
-                isActive={index === picked}
-                onClick={() => handlePick(index)}
+                isActive={index === activeIndex}
+                onClick={() => toggle(index)}
               />
             ))}
           </ul>
         </div>
       </div>
     </div>
-  )
+  );
 }
