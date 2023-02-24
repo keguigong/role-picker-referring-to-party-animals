931f068b88fbc406958ee27ef84d6d0d339ae60a
可圭共 <keguigong@gmail.com>
2023-02-24 23:47:18 +0800

    Update README.md

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath:
+    process.env.NODE_ENV === "production"
+      ? "/role-picker-referring-to-party-animals"
+      : "",
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
