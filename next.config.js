54b20fa1e571dbba147f489611b81ea538ed96c8
可圭共 <keguigong@gmail.com>
2023-02-28 13:04:00 +0800

    Remove basePath and assetPath

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
-const basePath =
-  process.env.NODE_ENV === "production"
-    ? "/role-picker-referring-to-party-animals"
-    : "";
-const assetPrefix =
-  process.env.NODE_ENV === "production"
-    ? "/role-picker-referring-to-party-animals"
-    : "/";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: basePath,
-  assetPrefix: assetPrefix,
 };
 
 module.exports = nextConfig;
--- role-picker/role-avatar.tsx
+++ role-picker/role-avatar.tsx
@@ -1,11 +1,6 @@
 import classNames from "classnames";
 import styles from "./styles.module.scss";
 
-const assetPrefix =
-  process.env.NODE_ENV === "production"
-    ? "/role-picker-referring-to-party-animals"
-    : "";
-
 const RoleAvatar = ({ name, onClick, isActive }: any) => {
   return (
     <li
@@ -19,14 +14,14 @@ const RoleAvatar = ({ name, onClick, isActive }: any) => {
         <picture>
           <img
             className={styles["avatar-bg"]}
-            src={`${assetPrefix}/party-animals/characters_avatar_hover.png`}
+            src={`party-animals/characters_avatar_hover.png`}
             alt="characters_avatar_hover"
           />
         </picture>
         <picture>
           <img
             className={styles["avatar-source"]}
-            src={`${assetPrefix}/party-animals/characters_${name}_avatar.png`}
+            src={`party-animals/characters_${name}_avatar.png`}
             alt={name}
           />
         </picture>
