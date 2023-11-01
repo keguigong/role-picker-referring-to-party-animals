2c67227e903acd4689663ec00d39e6024f15984c
keguigong <keguigong@gmail.com>
2023-11-01 11:23:38 +0800

    Update assets prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const basePath = process.env.NODE_ENV === "production" ? "/role-picker-referring-to-party-animals" : "";
+const assetPrefix = process.env.NODE_ENV === "production" ? "/role-picker-referring-to-party-animals" : "/";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath,
+  assetPrefix,
 };
 
 module.exports = nextConfig;
